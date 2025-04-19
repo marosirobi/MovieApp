fdbe976ebe0106043cd77c02fd1bc9007f9dffcd
unknown <18molnargabor18@gmail.com>
2025-04-19 14:10:17 +0200

    login_reg

---
+++ MovieApp/Datas/movieDB.Review.cs
@@ -0,0 +1,37 @@
+﻿namespace MovieDB
+{
+    public partial class Review {
+
+        public Review()
+        {
+            //generates the exact time
+            publish_date = DateTime.Now;
+            OnCreated();
+        }
+
+        
+
+        public short review_id { get; set; }
+
+        public string? content { get; set; }
+
+        public DateTime publish_date { get; set; }
+
+        public short stars { get; set; }
+
+        public short movie_id { get; set; }
+
+        public short user_id { get; set; }
+
+        public virtual Movie Movie { get; set; }
+
+        public virtual User User { get; set; }
+
+        #region Extensibility Method Definitions
+
+        partial void OnCreated();
+
+        #endregion
+    }
+
+}
---
+++ MovieApp/MVVM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Linq;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MovieApp.MVVM.Model;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace MovieApp.MVVM.ViewModel
+{
+    public class AuthViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegisterCommand { get; }
+
+        public AuthViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+            RegisterCommand = new RelayCommand(Register);
+
+            using var db = new MovieAppDBContext();
+            db.Database.EnsureCreated(); // létrehozza az adatbázist, ha nem létezik
+        }
+
+        private void Login()
+        {
+            using var db = new MovieAppDBContext();
+            var user = db.Users.FirstOrDefault(u => u.Username == Username && u.Password == Password);
+            MessageBox.Show(user != null ? "Sikeres bejelentkezés!" : "Hibás felhasználónév vagy jelszó.");
+        }
+
+        private void Register()
+        {
+            using var db = new MovieAppDBContext();
+            if (db.Users.Any(u => u.Username == Username))
+            {
+                MessageBox.Show("Ez a felhasználónév már foglalt.");
+                return;
+            }
+
+            db.Users.Add(new User { Username = Username, Password = Password });
+            db.SaveChanges();
+            MessageBox.Show("Sikeres regisztráció!");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
