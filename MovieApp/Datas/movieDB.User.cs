a1d0b91670a09ff695b66431e70fe42b08b9dc98
marosirobi <marosirobi666@gmail.com>
2025-04-22 18:37:36 +0200

    login_updated

---
+++ MovieApp/Datas/movieDB.User.cs
@@ -0,0 +1,27 @@
+﻿namespace MovieApp
+{
+    public partial class User {
+
+        public User()
+        {            
+            //init an empty "Reviews" list and a empty "Watchlists" list (default watchlist)
+            this.Watchlists = new List<Watchlist>();
+            this.Reviews = new List<Review>();
+            OnCreated();
+        }
+
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string passwd { get; set; }
+
+        public ICollection<Watchlist> Watchlists { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+
+        #region Extensibility Method Definitions
+
+        partial void OnCreated();
+
+        #endregion
+    }
+
+}
---
+++ MovieApp/MVVM/View/LoginView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MovieApp.MVVM.ViewModel;
+
+namespace MovieApp.MVVM.View
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            var viewModel = new LoginViewModel();
+            viewModel.OnLoginSuccess = (user) =>
+            {
+                // Create and show the main window
+                var mainWindow = new MainWindow();
+                mainWindow.DataContext = new MainViewModel { CurrentUser = user };
+                mainWindow.Show();
+
+                // Close the login window
+                this.Close();
+            };
+
+
+            DataContext = viewModel;
+        }
+        public void ClearPassword()
+        {
+            PasswordBox.Clear();
+        }
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is LoginViewModel viewModel)
+            {
+                viewModel.Password = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
