68c450d403cba263cdab105ecbec3de0dcfae82f
marosirobi <marosirobi666@gmail.com>
2025-04-22 16:25:43 +0200

    login_update

---
+++ MovieApp/Datas/movieDB.Review.cs
@@ -0,0 +1,37 @@
+﻿namespace MovieApp
+{
+    public partial class Review {
+
+        public Review()
+        {
+            //generates the exact time
+            publish_date = DateTime.Now;
+            OnCreated();
+        }
+
+        
+
+        public int review_id { get; set; }
+
+        public string? content { get; set; }
+
+        public DateTime publish_date { get; set; }
+
+        public int stars { get; set; }
+
+        public int movie_id { get; set; }
+
+        public int user_id { get; set; }
+
+        public virtual Movie Movie { get; set; }
+
+        public virtual User User { get; set; }
+
+        #region Extensibility Method Definitions
+
+        partial void OnCreated();
+
+        #endregion
+    }
+
+}
